cf91a3d0844b27cce1d6ef01bf4d65aa8ebbe742
Joyal A Johney <joyalajohny@gmail.com>
2019-07-08 15:54:21 +0530

    Login page UI created

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomePage());
+import 'login.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+void main() => runApp(MyApp());
 
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 1;
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cirgle',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Meetups"),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home), title: Text('Home')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.group), title: Text('Meetups')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.computer), title: Text('Projects')),
-            ],
-            currentIndex: _selectedIndex,
-            fixedColor: Colors.deepPurple,
-            onTap: _onItemTapped,
-          ),
-          body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Card(
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                    Text("stuff"),
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                  ],
-                ),
-              );
-            },
-          )),
+      title: "Cirgle",
+      home: LoginPage(),
     );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
