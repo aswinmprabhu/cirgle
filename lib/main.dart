5e00be4995266c319287b7c1da156e8b51cd5c2a
Joyal A Johney <joyalajohny@gmail.com>
2019-07-09 18:38:48 +0530

    Home page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'homepage.dart';
 import 'login.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Cirgle",
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
